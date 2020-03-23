d1c2bd3f278821386f091621683ff5434d9a6db7
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-03-24 00:34:21 +0100

    Add support for modifiers to dev tools remote prop

--- game/dev_tools/RemoteProp.gd
+++ game/dev_tools/RemoteProp.gd
@@ -3,13 +3,21 @@ class_name RemoteProp
 
 var remote_target: Node
 var remote_prop: String
+var remote_mods: Array
 
-func _init(target: Node, prop: String) -> void:
+func _init(target: Node, prop: String, mods := []) -> void:
     remote_target = target
     remote_prop = prop
+    remote_mods = mods
 
 func get_description() -> String:
     return remote_target.name + '.' + remote_prop
 
 func get_value() -> String:
-    return remote_target.get(remote_prop)
+    var val = remote_target.get(remote_prop)
+    for mod in remote_mods:
+        if mod == 'length':
+            val = val.length()
+        elif mod == 'round':
+            val = round(val * 100) / 100
+    return val
