f7f504cf1e293e8bb61326181b1a8bd62d6a9adb
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-03-24 22:11:23 +0100

    Implement smoothed movement

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -8,15 +8,21 @@ enum STATE {
     TRANSITION_OUT  #  moving back to normal mode
 }
 
-export var speed := 6.0
-export var transitions := 8.0
-export var release_force := 2.5
+export(float, EASE) var input_easing := 1.5
+export var speed := 5.0
+export var max_force := 28.0
+export var velocity_gain := 10.0
+export var transition_time_scale := 20.0
+export var release_force := 4.5
 
 var current_state: int = STATE.NORMAL
+
 var transition_target: Transform
 var transition_origin: Transform
 var transition_speed: float
 var transition_fraction: float
+
+onready var target_rotation := global_transform.basis.get_euler().y
 
 
 func _ready() -> void:
@@ -26,6 +32,7 @@ func _ready() -> void:
         RemoteProp.new(self, 'linear_velocity', ['length', 'round'])
     )
     DevTools.add_remote_value(RemoteProp.new(self, 'linear_velocity'))
+    DevTools.add_remote_value(RemoteProp.new(self, 'target_rotation'))
 
 
 func _input(event: InputEvent) -> void:
@@ -38,18 +45,34 @@ func _integrate_forces(state: PhysicsDirectBodyState) -> void:
     if current_state == STATE.NORMAL:
     
         var direction := Vector3()
-        direction.z += Input.get_action_strength('game_forward')
-        direction.z -= Input.get_action_strength('game_backward')
-        direction.x += Input.get_action_strength('game_left')
-        direction.x -= Input.get_action_strength('game_right')
+        direction.z += ease(Input.get_action_strength('game_forward'), input_easing)
+        direction.z -= ease(Input.get_action_strength('game_backward'), input_easing)
+        direction.x += ease(Input.get_action_strength('game_left'), input_easing)
+        direction.x -= ease(Input.get_action_strength('game_right'), input_easing)
         
-        # TODO: rotate without setting the complete transform
-        if state.linear_velocity:
-            state.transform = state.transform.looking_at(
-                state.transform.origin - state.linear_velocity, Vector3.UP
-            )
+        if direction:
+            var a := Vector3.FORWARD
+            var b := direction
+            var dot := a.x * b.x + (-a.z) * (-b.z)
+            var det := a.x * (-b.z) - (-a.z) * b.x
+            target_rotation = atan2(det, dot) + PI  # model is rotated by 180°
         
-        state.add_central_force(clamped(direction, 1) * speed)
+        var current_rotation := state.transform.basis.get_euler().y
+        var new_rotation := lerp_angle(
+            current_rotation,
+            target_rotation,
+            7 * state.step
+        )
+        state.transform.basis = state.transform.basis.rotated(
+            Vector3.UP,
+            new_rotation - current_rotation
+        )
+        
+        var desired := clamped(direction, 1) * speed
+        var error := desired - state.linear_velocity
+        var force := clamped(velocity_gain * error, max_force)
+        
+        state.add_central_force(force)
     
     elif current_state == STATE.TRANSITION_IN:
         
@@ -77,7 +100,7 @@ func on_detection(other: Area):
         transition_target = other.global_transform
         transition_origin = global_transform
         transition_fraction = 0
-        transition_speed = transitions * transition_target.origin.distance_to(
+        transition_speed = transition_time_scale * transition_target.origin.distance_to(
             transition_origin.origin
         )
 
